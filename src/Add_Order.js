0043fbb875089c6f1a4dd4c43026693958e73286
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-04 04:16:27 +0200

    Syntax Error :(

--- src/Add_Order.js
+++ src/Add_Order.js
@@ -24,19 +24,17 @@ const BASE_URL = "https://queue-backendser.onrender.com";
 
 
     // ✅ Generate Transaction ID (YYYYMMDD + 4 digits)
-    const generateTransactionID = () => {
-
-    const currentDate = formatDateDDMMYYYY();
+    const generateTransactionID = () => 
    //new Date().toISOString().slice(0, 10).replace(/-/g, "");
-
-        // Get date in DDMMYYYY format
+   // Get date in DDMMYYYY format
     const formatDateDDMMYYYY = () => {
     const date = new Date();
     const day = date.getDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear().toString();
     return `${day}${month}${year}`;
-};
+};    
+    const currentDate = formatDateDDMMYYYY();
     // Generate random sequence from 0000 to 9999 (use sequential logic if backend tracks numbers)
     const randomSequence = Math.floor(Math.random() * 10000).toString().padStart(4, "0");
     
