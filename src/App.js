301bf7cade45705c0445f3e06e4a72a6309065e0
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-09 10:27:09 +0200

    adding route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard"; // Adjust path if needed
 import TrackOrderPage from "./TrackOrderPage";
 import AddOrder from "./Add_Order";
+import AdminOrders from "./AdminOrders";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/queueStyles.css";
 
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Dashboard />} />
         <Route path="/track-order" element={<TrackOrderPage />} />
         <Route path="/add-order" element={<AddOrder />} />
+        <Route path="/admin-orders" element={<AdminOrders userRole="Admin" />} />
       </Routes>
     </Router>
   );
