d62169154ede5377f796ca39912a683cfa007fa2
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-20 14:13:23 +0200

    updated the path for import css

--- src/ColourCodeModal.jsx
+++ src/ColourCodeModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/ColourCodeModal.css";
+import "./styles/ColourCodeModal.css";
 
 const ColourCodeModal = ({ onSubmit, onCancel }) => {
   const [code, setCode] = useState("");
