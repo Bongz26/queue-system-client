368bbf77aad00349ff2629c8e975bcfa967b7c25
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-20 13:58:26 +0200

    fixed syntax error

--- src/LoginPopup.jsx
+++ src/LoginPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/LoginPopup.css";
+import "./styles/LoginPopup.css";
 
 const LoginPopup = ({ onLogin, onClose }) => {
   const [role, setRole] = useState("");
